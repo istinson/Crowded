b5b3be935b77cf954737516841690dfd17f5380f
istinson <ian.c.stinson@gmail.com>
2016-06-21 15:39:43 -0700

    [build] fix module loaders

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,30 @@ module.exports = {
       }
     }]
   },
+
+/*
+module: {
+    loaders: [
+      {
+        test : /\.js?/,
+        include : APP_DIR,
+        loader : 'babel'
+      },
+      {
+        test: /\.css$/,
+        include: APP_DIR,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      },
+      { 
+        test: /\.(png|jpg)$/,
+        include: APP_DIR,
+        loader: 'url-loader?limit=8192' 
+      }
+    ]
+  }
+
+*/
+
   resolve: {
     extensions: ['', '.js', '.jsx']
   }
