44f7aed1cedc73df9c929f9b07b4575155558b0e
istinson <ian.c.stinson@gmail.com>
2017-01-04 16:02:56 -0800

    [cleanup] cleanup pref-list

--- src/client/containers/pref-list.js
+++ src/client/containers/pref-list.js
@@ -1,27 +1,14 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {fetchCafeListByGeoloc} from '../actions/cafe-api';
-import {setNeighborhood, setPreferences, fetchCoordinates} from '../actions/index';
-import {Link, browserHistory} from 'react-router';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchCafeListByGeoloc } from '../actions/cafe-api';
+import { setNeighborhood, setPreferences, fetchCoordinates } from '../actions/index';
 
 class PrefList extends Component {
-  constructor (props)  {
-    super(props);
-    this.onPrefClick = this.onPrefClick.bind(this);
-    this.onPrefSubmit = this.onPrefSubmit.bind(this);
-    this.onNeighborhoodChange = this.onNeighborhoodChange.bind(this);
-    this.getCoords = this.getCoords.bind(this);
-  }
 
-  onPrefClick({ target, target: { dataset: { value } } } ) {
-    this.props.setPreferences(value);
-    if(this.props.pref.pref[value]) {
-      target.className = 'mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--1-col-phone clicked';
-    } else {
-      target.className = 'mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--1-col-phone unclicked';
-    }
-  }
+  static contextTypes = {
+    router: PropTypes.object
+  };
 
   getCoords() {
     if ("geolocation" in navigator) {
@@ -29,12 +16,23 @@ class PrefList extends Component {
         this.props.fetchCoordinates(position);
       });
     } else {
+      //Do I want to redirect to the neighborhood route?
       console.log("Sorry your browser is not supporting Geo Location");
     }
   }
 
   componentDidMount() {
-    this.getCoords();
+    this.getCoords.bind(this)();
+  }
+  
+  onPrefClick({ target, target: { dataset: { value } } } ) {
+    this.props.setPreferences(value);
+    const squareClass = 'mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--1-col-phone';
+    if(this.props.pref.pref[value]) {
+      target.className = `${squareClass} clicked`;
+    } else {
+      target.className = `${squareClass} unclicked`;
+    }
   }
 
   onPrefSubmit() { 
@@ -42,53 +40,80 @@ class PrefList extends Component {
       setTimeout(this.onPrefSubmit, 200);
     } else {
       this.props.fetchCafeListByGeoloc(this.props.term);
-      browserHistory.push('/cafes')
+      this.context.router.push('/cafes');
     }    
   }
 
-  onNeighborhoodChange(props) {
-    browserHistory.push('/neighborhood')
+  onNeighborhoodChange() {
+    this.context.router.push('/neighborhood');
   }
 
-  render() { 
-    let cardClass = 'mdl-cell mdl-cell--4-col unclicked';
+  render() {
+    const squareClass = "mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--1-col-phone";
+
     return (
       <div className="PrefList">
         <div className="div-holder">
           <div className="small-print">Location set closest to you</div>
           <div className="small-print-button">
-            <button type="submit" className="mdl-button mdl-button--raised mdl-button--accent mdl-js-button mdl-js-ripple-effect" onClick={this.onNeighborhoodChange}>Choose Neighborhood</button>
+            <button type="submit" 
+                    className="mdl-button mdl-button--raised mdl-button--accent mdl-js-button mdl-js-ripple-effect" 
+                    onClick={this.onNeighborhoodChange.bind(this)}>
+              Choose Neighborhood
+            </button>
           </div>
           <div className="search-button">
-            <button type="submit" className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--colored" onClick={this.onPrefSubmit}>Find Cafes</button>
+            <button type="submit" 
+                    className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--colored"
+                    onClick={this.onPrefSubmit.bind(this)}>
+              Find Cafes
+            </button>
           </div>
         </div>
         <div className="mdl-grid">
-          <div className="mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--1-col-phone unclicked" data-value='coffeeQuality' onClick={this.onPrefClick}>
+          <div className={`${squareClass} unclicked`} 
+               data-value='coffeeQuality' 
+               onClick={this.onPrefClick.bind(this)}>
             Coffee
           </div>
-          <div className="mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--1-col-phone unclicked" data-value='ambiance' onClick={this.onPrefClick}>
+          <div className={`${squareClass} unclicked`} 
+               data-value='ambiance' 
+               onClick={this.onPrefClick.bind(this)}>
             Ambiance
           </div>
-          <div className="mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--1-col-phone unclicked" data-value='rating' onClick={this.onPrefClick}>
+          <div className={`${squareClass} unclicked`} 
+               data-value='rating' 
+               onClick={this.onPrefClick.bind(this)}>
             Rating
           </div>
-          <div className="mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--1-col-phone unclicked" data-value='seats' onClick={this.onPrefClick}>
+          <div className={`${squareClass} unclicked`} 
+               data-value='seats' 
+               onClick={this.onPrefClick.bind(this)}>
             Seats
           </div>
-          <div className="mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--1-col-phone unclicked" data-value='outlets' onClick={this.onPrefClick}>
+          <div className={`${squareClass} unclicked`} 
+               data-value='outlets' 
+               onClick={this.onPrefClick.bind(this)}>
             Outlets
           </div>
-          <div className="mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--1-col-phone unclicked" data-value='bathroomQuality' onClick={this.onPrefClick}>
+          <div className={`${squareClass} unclicked`} 
+               data-value='bathroomQuality' 
+               onClick={this.onPrefClick.bind(this)}>
             Bathrooms
           </div>
-          <div className="mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--1-col-phone unclicked" data-value='line' onClick={this.onPrefClick}>
+          <div className={`${squareClass} unclicked`} 
+               data-value='line' 
+               onClick={this.onPrefClick.bind(this)}>
             Line
           </div>
-          <div className="mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--1-col-phone unclicked" data-value='noise' onClick={this.onPrefClick}>
+          <div className={`${squareClass} unclicked`} 
+               data-value='noise' 
+               onClick={this.onPrefClick.bind(this)}>
             Noise
           </div>
-          <div className="mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--1-col-phone unclicked" data-value='price' onClick={this.onPrefClick}>
+          <div className={`${squareClass} unclicked`} 
+               data-value='price' 
+               onClick={this.onPrefClick.bind(this)}>
             Price
           </div>
         </div>
