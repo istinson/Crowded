2ef0646c8126c8ec3ca2a34b13d4bef2efe96433
istinson <ian.c.stinson@gmail.com>
2017-01-07 22:38:28 -0800

    [cleanup] refactor neighborhood-list

--- src/client/containers/neighborhood-list.js
+++ src/client/containers/neighborhood-list.js
@@ -1,68 +1,80 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes } from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {setNeighborhood} from '../actions/index';
-import {Link, browserHistory} from 'react-router';
 import {fetchCafeListByGeoloc} from '../actions/cafe-api';
 
 class NeighborhoodList extends Component {
-  constructor (props)  {
-    super(props);
-    this.onNeigbhorhoodClick = this.onNeigbhorhoodClick.bind(this);
-    this.onPrefSubmit = this.onPrefSubmit.bind(this);
-    this.onHoodSubmit = this.onHoodSubmit.bind(this);  }
+  static contextTypes = {
+    router: PropTypes.object
+  };
+  
+  onPrefRoute(){
+    this.context.router.push('/');
+  }
+  
+  onHoodSubmit({ target: { dataset: { value } } }) {
+    this.props.setNeighborhood(value);
+    this.props.fetchCafeListByGeoloc(value);
+    this.context.router.push('/cafes');
+  }
+  
+  render() {
+    const squareClass = 'mdl-cell mdl-cell--4-col';
 
-  onNeigbhorhoodClick(event) {
-    this.props.setNeighborhood(event.target.value);
-    let flag = this.props.pref.pref[event.target.value];
-    if (flag)  {
-      event.target.className = 'mdl-cell mdl-cell--4-col clicked';
-      flag === false;
-    } else {
-      event.target.className = 'mdl-cell mdl-cell--4-col unclicked';
-      flag === true;
-    }
-  }
-  onPrefSubmit(props) {
-    browserHistory.push('/')
-  }
-  onHoodSubmit(event) {
-    this.props.fetchCafeListByGeoloc(event.target.value);
-    browserHistory.push('/cafes');
-  }
- 
-  render() { 
     return (
       <div className="NeighborhoodList">
         <div className="search-button-neighborhood">
-              <button type="submit" className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--colored" onClick={this.onPrefSubmit}>Back to Prefs</button>
+              <button type="submit"
+                      className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--colored"
+                      onClick={this.onPrefRoute.bind(this)}>
+                Back to Prefs
+              </button>
         </div>
         <div className="mdl-grid">
-          <div className="mdl-cell mdl-cell--4-col unclicked" value='37.759481,-122.414968' onClick={this.onHoodSubmit}>
+          <div className={`${squareClass} unclicked`}
+               data-value='37.759481,-122.414968'
+               onClick={this.onHoodSubmit.bind(this)}>
             Mission
           </div>
-          <div className="mdl-cell mdl-cell--4-col unclicked" value='37.762462, -122.434905' onClick={this.onHoodSubmit}>
+          <div className={`${squareClass} unclicked`}
+               data-value='37.762462, -122.434905'
+               onClick={this.onHoodSubmit.bind(this)}>
             Castro
           </div>
-          <div className="mdl-cell mdl-cell--4-col unclicked" value='37.750581, -122.486192' onClick={this.onHoodSubmit}>
+          <div className={`${squareClass} unclicked`}
+               data-value='37.750581, -122.486192'
+               onClick={this.onHoodSubmit.bind(this)}>
             Sunset
           </div>
-          <div className="mdl-cell mdl-cell--4-col unclicked" value='37.778871, -122.478834' onClick={this.onHoodSubmit}>
+          <div className={`${squareClass} unclicked`}
+               data-value='37.778871, -122.478834'
+               onClick={this.onHoodSubmit.bind(this)}>
             Richmond
           </div>
-          <div className="mdl-cell mdl-cell--4-col unclicked" value='37.803224, -122.437432' onClick={this.onHoodSubmit}>
+          <div className={`${squareClass} unclicked`}
+               data-value='37.803224, -122.437432'
+               onClick={this.onHoodSubmit.bind(this)}>
             Marina
           </div>
-          <div className="mdl-cell mdl-cell--4-col unclicked" value='37.804101, -122.408153' onClick={this.onHoodSubmit}>
+          <div className={`${squareClass} unclicked`}
+               data-value='37.804101, -122.408153'
+               onClick={this.onHoodSubmit.bind(this)}>
             North Beach
           </div>
-          <div className="mdl-cell mdl-cell--4-col unclicked" value='37.770061, -122.444712' onClick={this.onHoodSubmit}>
+          <div className={`${squareClass} unclicked`}
+               data-value='37.770061, -122.444712'
+               onClick={this.onHoodSubmit.bind(this)}>
             Haight
           </div>
-          <div className="mdl-cell mdl-cell--4-col unclicked" value='37.778915, -122.398127' onClick={this.onHoodSubmit}>
+          <div className={`${squareClass} unclicked`}
+               data-value='37.778915, -122.398127'
+               onClick={this.onHoodSubmit.bind(this)}>
             FiDi / Soma
           </div>
-          <div className="mdl-cell mdl-cell--4-col unclicked" value='37.751860, -122.450125' onClick={this.onHoodSubmit}>
+          <div className={`${squareClass} unclicked`}
+               data-value='37.751860, -122.450125'
+               onClick={this.onHoodSubmit.bind(this)}>
             Twin Peaks
           </div>
         </div>
